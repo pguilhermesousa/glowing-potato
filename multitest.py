d03d198d4de39aa8b148207a5a5e5e3e711ed668
pguilhermesousa <96555353+pguilhermesousa@users.noreply.github.com>
2021-12-23 14:59:41 +0000

    Add files via upload

--- multitest.py
+++ multitest.py
@@ -1,5 +1,4 @@
 import base64
-import sys
 import imgurAPIrequests
 import requests
 import time
@@ -10,7 +9,6 @@ from threading import Thread
 
 
 def uploadFunction(imgOriginal, imgSecret, room):
-    tsStart = time.time()
     imageOriginal = imgOriginal
     imageSecret = imgSecret
 
@@ -37,12 +35,8 @@ def uploadFunction(imgOriginal, imgSecret, room):
     payload = {"text": result}
     req = requests.put(url, data=payload)
 
-    tsEnd = time.time()
-    tsDiff = tsEnd - tsStart
-    print(tsDiff)
 
 def downloadFunction(room):
-    tsStart = time.time()
     mainUrl = "http://dontpad.com/"
     room = room
 
@@ -64,15 +58,22 @@ def downloadFunction(room):
     secretMessage = decrypted.decode("utf-8")
     print(secretMessage)
 
+
+def testFunction(imgOriginal, imgSecret, room):
+    tsStart = time.time()
+
+    uploadFunction(imgOriginal, imgSecret, room)
+    downloadFunction(room)
+
     tsEnd = time.time()
     tsDiff = tsEnd - tsStart
     print(tsDiff)
 
 
-t1 = Thread(target=uploadFunction("original/big.png", "testingwithhal1"))
-t2 = Thread(target=uploadFunction("original/medium.png", "testingwithhal2"))
-t3 = Thread(target=uploadFunction("original/small.png", "testingwithhal3"))
+#t1 = Thread(target=testFunction("original/small.png", "smallSecret.png", "testingwithhal1"))
+#t2 = Thread(target=testFunction("original/medium.png", "mediumSecret.png", "testingwithhal2"))
+t3 = Thread(target=testFunction("original/big.png", "bigSecret.png", "testingwithhal3"))
 
-t1.start()
-t2.start()
+#t1.start()
+#t2.start()
 t3.start()
